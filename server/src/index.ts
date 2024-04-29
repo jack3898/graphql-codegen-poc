54a7eef6f9835f73edac6d29fb9e1cddfd94c65a
Jack Wright <28375223+jack3898@users.noreply.github.com>
2024-04-29 21:11:12 +0100

    add apollo express middleware

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,17 +1,35 @@
 import { ApolloServer } from '@apollo/server';
-import { startStandaloneServer } from '@apollo/server/standalone';
 import { resolvers } from './resolvers.js';
 import { readFile } from 'node:fs/promises';
+import express from 'express';
+import http from 'node:http';
+import { ApolloServerPluginDrainHttpServer } from '@apollo/server/plugin/drainHttpServer';
+import cors from 'cors';
+import { expressMiddleware } from '@apollo/server/express4';
 
 const typeDefs = await readFile('src/type-defs.graphql').then((buf) => buf.toString('utf-8'));
 
-const server = new ApolloServer({
+const port = 3000;
+const server = express();
+const httpServer = http.createServer(server);
+
+const apolloServer = new ApolloServer({
   typeDefs,
-  resolvers
+  resolvers,
+  plugins: [ApolloServerPluginDrainHttpServer({ httpServer })]
 });
 
-const { url } = await startStandaloneServer(server, {
-  listen: { port: 3000 }
-});
+await apolloServer.start();
 
-console.log(`🚀  Server ready at: ${url}`);
+server.use(
+  '/graphql',
+  cors<cors.CorsRequest>(),
+  express.json(),
+  expressMiddleware(apolloServer, {
+    context: async ({ req }) => ({ token: req.headers.token })
+  })
+);
+
+await new Promise<void>((resolve) => httpServer.listen({ port: 3000 }, resolve));
+
+console.log(`🚀  Server ready on port ${port}!`);
